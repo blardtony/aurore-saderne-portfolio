14d76eaf48fefe75423c37991cf0d2fd877a3ee8
Tony Blard <tonyblard55@gmail.com>
2024-07-16 15:45:52 +0200

    Add background custom color Add Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 function App() {
-  return <></>;
+  return <div className="bg-background">
+    <div className="mx-auto container">
+      <Hero />
+    </div>
+  </div>;
 }
 
 export default App;
