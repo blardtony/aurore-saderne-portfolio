fd8f16d1baa1f7122451d68d2e5846eb13e2a1fa
Tony Blard <tonyblard55@gmail.com>
2024-07-16 20:40:00 +0200

    Add min height set to dvh

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,7 +1,9 @@
 import { PropsWithChildren } from "react";
 
 const Section = ({ children }: PropsWithChildren) => {
-  return <section className="px-6 py-4">{children}</section>;
+  return (
+    <section className="min-h-dvh content-center px-6 py-4">{children}</section>
+  );
 };
 
 export default Section;
