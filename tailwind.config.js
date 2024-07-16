f6ffdc5669aae449fb3f200c6f1d764ad4120a43
Tony Blard <tonyblard55@gmail.com>
2024-07-16 18:01:37 +0200

    Add green 100 and green 200, white and black colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      green: "rgb(9, 125, 76)",
+      white: "rgb(255, 255, 255)",
+      black: "rgb(0, 0, 0)",
+      green: { 200: "rgb(9, 125, 76)", 100: "#0a8954" },
       blue: "rgb(0, 78, 173)",
       yellow: "rgb(239, 189, 64)",
       background: "rgb(241, 234, 209)",
