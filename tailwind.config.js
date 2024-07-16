d97543c7db89e1579e1e139373c1a941f38b5276
Tony Blard <tonyblard55@gmail.com>
2024-07-16 15:04:23 +0200

    Install react project with Typescript Add tailwindcss, prettier tailwind Custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      green: "rgb(9, 125, 76)",
+      blue: "rgb(0, 78, 173)",
+      yellow: "rgb(239, 189, 64)",
+      background: "rgb(241, 234, 209)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
