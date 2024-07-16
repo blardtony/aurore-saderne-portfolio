781842440e828689c629ee28978959d30d409e3c
Tony Blard <tonyblard55@gmail.com>
2024-07-16 20:39:27 +0200

    Rename Hero folder by hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import Hero from "./components/Hero/Hero";
+import Hero from "./components/hero/Hero";
 
 function App() {
-  return <div className="bg-background">
-    <div className="mx-auto container">
-      <Hero />
+  return (
+    <div className="bg-background">
+      <div className="container mx-auto">
+        <Hero />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
