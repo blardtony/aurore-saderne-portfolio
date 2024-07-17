6b6a1262e405819bb3be119c045b5163d1dbe008
Tony Blard <tonyblard55@gmail.com>
2024-07-17 14:11:34 +0200

    Add navbar in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import SchoolExperience from "./components/experience/SchoolExperience";
 import WorkExperience from "./components/experience/WorkExperience";
+import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 
 function App() {
   return (
     <div className="bg-background">
+      <Header />
       <div className="container mx-auto">
         <Hero />
         <WorkExperience />
