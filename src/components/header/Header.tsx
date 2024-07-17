8f69d558733e05fb04d0e52d47b3139275ec35f6
Tony Blard <tonyblard55@gmail.com>
2024-07-17 22:19:05 +0200

    Remove unused div

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,56 +17,54 @@ const Header = () => {
   };
   window.addEventListener("scroll", handleScroll);
   return (
-    <div>
-      <nav
+    <nav
+      className={
+        "fixed left-0 top-0 z-10 flex w-full items-center justify-between bg-background px-6 py-2 transition-all duration-300 ease-in-out " +
+        (isScrolling ? "h-24 shadow-md" : "h-28 shadow-none")
+      }
+    >
+      <a className="h-full">
+        <img className="h-full" src={hero} />
+      </a>
+      {isOpen ? (
+        <XMarkIcon
+          className="z-30 h-10 cursor-pointer text-white"
+          onClick={() => toggleMenu()}
+        />
+      ) : (
+        <Bars3BottomLeftIcon
+          className="z-30 h-10 cursor-pointer"
+          onClick={() => toggleMenu()}
+        />
+      )}
+      <ul
         className={
-          "fixed left-0 top-0 z-10 flex w-full items-center justify-between bg-background px-6 py-2 transition-all duration-300 ease-in-out " +
-          (isScrolling ? "h-24 shadow-md" : "h-28 shadow-none")
+          "fixed top-0 z-20 h-full w-full content-center bg-black text-center text-white transition-all duration-500 ease-in-out md:w-80 " +
+          (isOpen ? "right-0" : "-right-full")
         }
       >
-        <a className="h-full">
-          <img className="h-full" src={hero} />
-        </a>
-        {isOpen ? (
-          <XMarkIcon
-            className="z-30 h-10 cursor-pointer text-white"
-            onClick={() => toggleMenu()}
-          />
-        ) : (
-          <Bars3BottomLeftIcon
-            className="z-30 h-10 cursor-pointer"
-            onClick={() => toggleMenu()}
-          />
-        )}
-        <ul
-          className={
-            "fixed top-0 z-20 h-full w-full content-center bg-black text-center text-white transition-all duration-500 ease-in-out md:w-80 " +
-            (isOpen ? "right-0" : "-right-full")
-          }
-        >
-          <li className="upppercase py-6 text-2xl font-bold">
-            <a href="#hero" onClick={toggleMenu}>
-              Accueil
-            </a>
-          </li>
-          <li className="upppercase py-6 text-2xl font-bold">
-            <a href="#work" onClick={toggleMenu}>
-              Expériences
-            </a>
-          </li>
-          <li className="upppercase py-6 text-2xl font-bold">
-            <a href="#school" onClick={toggleMenu}>
-              Parcours scolaire
-            </a>
-          </li>
-          <li className="upppercase py-6 text-2xl font-bold">
-            <a href="#contact" onClick={toggleMenu}>
-              Contactez-moi
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        <li className="upppercase py-6 text-2xl font-bold">
+          <a href="#hero" onClick={toggleMenu}>
+            Accueil
+          </a>
+        </li>
+        <li className="upppercase py-6 text-2xl font-bold">
+          <a href="#work" onClick={toggleMenu}>
+            Expériences
+          </a>
+        </li>
+        <li className="upppercase py-6 text-2xl font-bold">
+          <a href="#school" onClick={toggleMenu}>
+            Parcours scolaire
+          </a>
+        </li>
+        <li className="upppercase py-6 text-2xl font-bold">
+          <a href="#contact" onClick={toggleMenu}>
+            Contactez-moi
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
