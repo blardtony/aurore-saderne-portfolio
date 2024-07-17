2c5f461c7f8d7f796c8dbc4aa7e61d6a567154a2
Tony Blard <tonyblard55@gmail.com>
2024-07-17 11:50:41 +0200

    Create ExperienceItem component

---
+++ src/components/common/ExperienceItem.tsx
@@ -0,0 +1,31 @@
+type Experience = {
+  date: string;
+  title: string;
+  location: string;
+  description: string;
+};
+type ExperienceItemProps = {
+  experience: Experience;
+  icon: React.ReactNode;
+};
+
+const ExperienceItem = ({ experience, icon }: ExperienceItemProps) => {
+  return (
+    <div className="relative max-w-xl pl-20 pt-3">
+      <div className="absolute left-6 top-0 h-full w-2 bg-yellow"></div>
+      <div className="absolute left-0 top-0 h-14 w-14 rounded-full bg-yellow p-1">
+        {icon}
+      </div>
+      <div className="mb-3 inline-block rounded-2xl bg-yellow px-4 py-1 font-bold text-black">
+        {experience.date}
+      </div>
+      <h3 className="mb-1 text-2xl font-bold">{experience.title}</h3>
+      <div className="mb-4 text-lg font-bold text-green-200">
+        {experience.location}
+      </div>
+      <p className="mb-5">{experience.description}</p>
+    </div>
+  );
+};
+
+export default ExperienceItem;
