2c8f633ff25169255e25f156dd9a8c119cef98cd
Tony Blard <tonyblard55@gmail.com>
2024-07-17 14:58:42 +0200

    Create Contact component

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,12 @@
+import Section from "../common/Section";
+import Title from "../common/Title";
+
+const Contact = () => {
+  return (
+    <Section id={"contact"}>
+      <Title>Contactez-moi</Title>
+    </Section>
+  );
+};
+
+export default Contact;
