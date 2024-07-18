fbb6826b39fd9bb4c3ec0814a39dbe3af46c4f0a
Tony Blard <tonyblard55@gmail.com>
2024-07-18 07:54:42 +0200

    Add h-10 in Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,11 +4,11 @@ const Button = ({ children, url }: PropsWithChildren<{ url: string }>) => {
   return (
     <a
       href={url}
-      className="text-white bg-green-200 hover:bg-green-100 focus-visible:ring-green-200 focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md px-4 py-2 font-medium ring-offset-background transition-colors duration-300 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      className="focus-visible:ring-ring inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md bg-green-200 px-4 py-2 font-medium text-white ring-offset-background transition-colors duration-300 ease-in-out hover:bg-green-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-200 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
     >
       {children}
     </a>
   );
-};  
+};
 
 export default Button;
