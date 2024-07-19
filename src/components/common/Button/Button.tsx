a475668d4292abf3f0804930adbc7f6cf4d44d94
Tony Blard <tonyblard55@gmail.com>
2024-07-19 14:06:18 +0200

    Add viant size classname and all props in Button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,23 @@
 import { VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes } from "react";
 import buttonVariants from "./buttonVariants";
+import { cn } from "../../../libs/utils";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
 
-const Button = ({ children }: ButtonProps) => {
+const Button = ({
+  children,
+  size,
+  variant,
+  className,
+  ...props
+}: ButtonProps) => {
   return (
-    <button className="focus-visible:ring-ring inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md bg-green-200 px-4 py-2 font-medium text-white ring-offset-background transition-colors duration-300 ease-in-out hover:bg-green-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-200 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+    <button
+      {...props}
+      className={cn(buttonVariants({ variant, size }), className)}
+    >
       {children}
     </button>
   );
