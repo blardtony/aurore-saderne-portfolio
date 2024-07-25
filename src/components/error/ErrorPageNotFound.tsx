736259baebcd6b541610cfaebf05ed3083b27ef8
Tony Blard <tonyblard55@gmail.com>
2024-07-25 08:59:58 +0200

    Adapt font for mobile device

--- src/components/error/ErrorPageNotFound.tsx
+++ src/components/error/ErrorPageNotFound.tsx
@@ -4,13 +4,13 @@ const ErrorPageNotFound = () => {
   return (
     <div className="container mx-auto grid h-dvh place-content-center px-6 py-2">
       <div className="text-center">
-        <h1 className="mb-4 font-playwrite text-8xl font-bold text-green-200">
+        <h1 className="mb-4 font-playwrite text-7xl font-bold text-green-200 md:text-8xl">
           404
         </h1>
-        <p className="mb-4 text-2xl">
+        <p className="mb-4 text-lg md:text-2xl">
           La page que vous recherchez semble introuvable !
         </p>
-        <p className="mb-6 text-base">
+        <p className="mb-6 text-sm md:text-base">
           Retournez à l'accueil pour trouver ce que vous cherchez.
         </p>
         <LinkButton to="/">Retour à l'accueil</LinkButton>
