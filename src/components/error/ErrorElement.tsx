04334023b6c77088d2379354d652129c7eea2a9b
Tony Blard <tonyblard55@gmail.com>
2024-07-25 09:12:31 +0200

    Fix error 404

--- src/components/error/ErrorElement.tsx
+++ src/components/error/ErrorElement.tsx
@@ -1,17 +1,20 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-import ErrorPageNotFound from "./ErrorPageNotFound";
-
+type RouteError = {
+  message?: string;
+};
 const ErrorElement = () => {
-  const error = useRouteError();
+  const error: RouteError | unknown = useRouteError();
   if (isRouteErrorResponse(error)) {
-    if (error.status === 404) {
-      return <ErrorPageNotFound />;
-    }
+    return (
+      <div>
+        <h1>Oops!</h1>
+        <h2>{error.status}</h2>
+        <p>{error.statusText}</p>
+        {error.data?.message && <p>{error.data.message}</p>}
+      </div>
+    );
+  } else {
+    return <div>Oops</div>;
   }
-  return (
-    <div>
-      <h1>Oops, something went wrong!</h1>
-    </div>
-  );
 };
 export default ErrorElement;
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Index from "./routes/Index";
 import Layout from "./components/common/Layout";
 import ErrorElement from "./components/error/ErrorElement";
+import ErrorPageNotFound from "./components/error/ErrorPageNotFound";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
         path: "",
         element: <Index />,
       },
+      {
+        path: "*",
+        element: <ErrorPageNotFound />,
+      }
     ],
   },
 ]);
